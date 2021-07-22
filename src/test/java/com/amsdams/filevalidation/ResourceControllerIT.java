5fe7c18b1aa792e5217310b0a108bfc04aa449a6
amsdams <julien.langeveld@gmail.com>
2021-07-22 15:34:16 +0200

    updated some unit tests

--- src/test/java/com/amsdams/filevalidation/ResourceControllerIT.java
+++ src/test/java/com/amsdams/filevalidation/ResourceControllerIT.java
@@ -2,6 +2,8 @@ package com.amsdams.filevalidation;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
@@ -20,24 +22,35 @@ public class ResourceControllerIT {
 	
 	@Test
 	void doHome() throws Exception {
-		mockMvc.perform(get(RESOURCE_MAPPING+"hello")).andDo(print()).andExpect(status().isOk());		
+		mockMvc.perform(get(RESOURCE_MAPPING+"hello")).andDo(print()).andExpect(status().isOk()).andExpect(content().string("Hello Docker World"));		
 	}
 	
 	@Test
 	void doGetResources() throws Exception {
-		mockMvc.perform(get(RESOURCE_MAPPING)).andDo(print()).andExpect(status().isOk());		
+		mockMvc.perform(get(RESOURCE_MAPPING)).andDo(print()).andExpect(status().isOk())
+			.andExpect(jsonPath("$[0].name").value("file_example_MP3_700KB.mp3"))
+			.andExpect(jsonPath("$[0].mediaType.name").value("audio/mpeg"))
+			.andExpect(jsonPath("$[0].codecType[0].name").value("mp3"))
+			.andExpect(jsonPath("$[1].name").value("file_example_MP4_480_1_5MG.mp4"))
+			.andExpect(jsonPath("$[1].mediaType.name").value("video/mp4"))
+			.andExpect(jsonPath("$[1].codecType[0].name").value("h264"))
+			.andExpect(jsonPath("$[1].codecType[1].name").value("aac"));
+
 	}
 	
 	@Test
 	void doGetMediaType() throws Exception {
 		String name = "file_example_MP4_480_1_5MG.mp4";
-		mockMvc.perform(get(RESOURCE_MAPPING+"{name}/media-type", name)).andDo(print()).andExpect(status().isOk());		
+		mockMvc.perform(get(RESOURCE_MAPPING+"{name}/media-type", name)).andDo(print()).andExpect(status().isOk())
+			.andExpect(jsonPath("$.name").value("video/mp4"));	
 	}
 	
 	@Test
 	void doGetCodecTypes() throws Exception {
 		String name = "file_example_MP4_480_1_5MG.mp4";
-		mockMvc.perform(get(RESOURCE_MAPPING+"{name}/codec-types", name)).andDo(print()).andExpect(status().isOk());		
+		mockMvc.perform(get(RESOURCE_MAPPING+"{name}/codec-types", name)).andDo(print())
+			.andExpect(status().isOk()).andExpect(jsonPath("$[0].name").value("h264"))	
+			.andExpect(status().isOk()).andExpect(jsonPath("$[1].name").value("aac"));
 
 	}
 	
